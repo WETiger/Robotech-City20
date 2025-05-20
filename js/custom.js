b642d882e41306a7082e05d12097ef8726dc15d9
WebTiget <billal8254@gmail.com>
2025-05-20 10:27:14 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+const carousel = new bootstrap.Carousel('#myCarousel')
+const myCarouselElement = document.querySelector('#myCarousel')
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 1000,
+  touch: false
+})
+
